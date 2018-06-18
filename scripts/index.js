9b4e4e1f1787e85787c50b3a68afced7d676c2bb
cn-90 <chr.revel@gmail.com>
2018-06-18 08:29:27 -0700

    Finished setting up HTML

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const Github = require('./Github');
 
-let gitHub = new Github();
+const gitHub = new Github();
 
 app.get('/', (req, res) => {
   res.send("Hello from the main page.")
